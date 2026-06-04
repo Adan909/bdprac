8d218fe3be89e109c371e61a29eff6b94693f71a
Adan909 <jorgeot@uamv.edu.ni>
2026-06-04 07:43:24 -0600

    feat(db): añadi schemas, junto con las tablas medicos, pacientes y especialidades

--- querypractica.sql
+++ querypractica.sql
@@ -2,6 +2,46 @@ use master
 go
 
 create database HospitalDB
+go
 
+
+CREATE SCHEMA Hospital;
+go
+
+CREATE SCHEMA Personal
+GO
+
+CREATE SCHEMA Gestion;
+go
+
+
+create table Hospital.Pacientes (
+	IdPaciente int PRIMARY KEY IDENTITY(1,1),
+	Nombre nvarchar(100) NOT NULL,
+	Apellido nvarchar(100) NOT NULL,
+	FechaNacimiento datetime NOT NULL,
+	created_at datetime DEFAULT GETDATE(),
+	updated_at datetime DEFAULT GETDATE(),
+	deleted_at datetime
+);
+
+create table Personal.Medicos (
+	IdMedico int PRIMARY KEY IDENTITY(1,1),
+	Nombre nvarchar(100) NOT NULL,
+	Apellido nvarchar(100) NOT NULL,
+	Especialidad nvarchar(100) NOT NULL,
+	created_at datetime DEFAULT GETDATE(),
+	updated_at datetime DEFAULT GETDATE(),
+	deleted_at datetime
+);
+
+create table Personal.Especialidades (
+	IdEspecialidad int PRIMARY KEY IDENTITY(1,1),
+	Nombre nvarchar(100) NOT NULL,
+	created_at datetime DEFAULT GETDATE(),
+	updated_at datetime DEFAULT GETDATE(),
+	deleted_at datetime
+);
+	
 
 
