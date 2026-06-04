33ec36e79a4ecf4f1cac304ca72ef122895ee5f0
Adan909 <jorgeot@uamv.edu.ni>
2026-06-04 08:28:08 -0600

    fix(db): Se arreglaron y añadieron varias caracteristicas de las tablas con alter

--- querypractica.sql
+++ querypractica.sql
@@ -36,6 +36,7 @@ create table Personal.Medicos (
 	IdMedico int PRIMARY KEY IDENTITY(1,1),
 	Nombre nvarchar(100) NOT NULL,
 	Apellido nvarchar(100) NOT NULL,
+	salario decimal(18, 2) NOT NULL Constraint CK_Medico_Salario CHECK (salario > 0),
 	Correo nvarchar(100) NOT NULL Constraint UQ_CorreoMedico UNIQUE,
 	IdEspecialidad int NOT NULL,
 	created_at datetime DEFAULT GETDATE(),
@@ -99,6 +100,39 @@ create table Gestion.Medicamentos (
 	updated_at datetime DEFAULT GETDATE(),
 	deleted_at datetime
 );
+
+
+ALTER TABLE Gestion.Tratamientos ADD IdPaciente INT;
+ALTER TABLE Gestion.Tratamientos ADD CONSTRAINT FK_Tratamientos_Pacientes 
+    FOREIGN KEY (IdPaciente) REFERENCES Hospital.Pacientes(IdPaciente);
+
+ALTER TABLE Gestion.Medicamentos ADD IdTratamiento INT;
+ALTER TABLE Gestion.Medicamentos ADD CONSTRAINT FK_Medicamentos_Tratamientos 
+    FOREIGN KEY (IdTratamiento) REFERENCES Gestion.Tratamientos(IdTratamiento);
+
+
+ALTER TABLE Hospital.Habitaciones ADD IdPaciente INT;
+ALTER TABLE Hospital.Habitaciones ADD CONSTRAINT FK_Habitaciones_Pacientes 
+    FOREIGN KEY (IdPaciente) REFERENCES Hospital.Pacientes(IdPaciente);
+
+
+
+
+ALTER TABLE Hospital.Pacientes ADD Telefono NVARCHAR(20);
+ALTER TABLE Hospital.Pacientes ADD Direccion NVARCHAR(150);
+ALTER TABLE Hospital.Pacientes ADD Genero CHAR(1);
+ALTER TABLE Hospital.Pacientes ADD Tipo_Sangre VARCHAR(5);
+ALTER TABLE Hospital.Pacientes ADD Fecha_Nacimiento DATE;
+ALTER TABLE Hospital.Pacientes ALTER COLUMN Nombre NVARCHAR(150) NOT NULL;
+ALTER TABLE Hospital.Pacientes ALTER COLUMN Direccion NVARCHAR(250);
+ALTER TABLE Personal.Medicos ADD Experiencia INT;
+ALTER TABLE Personal.Medicos ADD Turno NVARCHAR(20);
+ALTER TABLE Personal.Medicos ADD Observaciones NVARCHAR(255);
+ALTER TABLE Personal.Medicos DROP COLUMN Observaciones;
+ALTER TABLE Gestion.Citas ADD Estado NVARCHAR(20) DEFAULT 'Pendiente';
+ALTER TABLE Gestion.Citas ADD Costo_Consulta DECIMAL(18,2);
+ALTER TABLE Gestion.Citas ALTER COLUMN Costo_Consulta DECIMAL(12,2);
+ALTER TABLE Hospital.Habitaciones ADD Disponibilidad BIT DEFAULT 1;
 
 
 
